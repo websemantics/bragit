87b3826e4cabdde6369b460d05e3149b6cf9cff3
Adnan <adnan@websemantics.ca>
2016-05-04 18:10:53 -0400

    Working on checking if css is loaded

--- semanti.js
+++ semanti.js
@@ -20,9 +20,12 @@
     root.Semanti = factory(root.jQuery)
   }
 }(this, function ($) {
-  var root = this || global
-  var me = {VERSION: '0.1.1'}
+  var root     = this || global
+  var doc      = root.document
+  var me       = {VERSION: '0.1.1'}
+  var debug    = true
   var base_uri = 'https://api.github.com/repos/'
+  var semantic = ['semantic.min.css', 'semantic.css']
 
   /* Library defaults, can be changed using the 'defaults' member method,
 
@@ -37,7 +40,7 @@
 
     - css (object), contains information about css styles to be injected into the document at run-time
     * version (string), semantic-ui version
-    * inject (boolean), if true, inject semantic-ui styles
+    * inject (boolean), if true, inject semantic-ui styles (false by default)
     * uri (string), uri template for the cdn used
     * modules (array), list of semantic-ui components
     * custom (string), any custom css styles if css has been injected
@@ -69,10 +72,10 @@
     },
     css: {
       version: '2.1.8',
-      inject: true,
+      inject: false,
       uri: 'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/{{version}}/components/{{module}}.min.css',
       modules: ['button', 'icon', 'label'],
-      custom : ''
+      custom : null
     }
   }
 
@@ -88,14 +91,7 @@
   */
 
   me.defaults = function (opts) {
-    if (typeof opts === 'object' && opts !== null) {
-      for (var prop in opts) {
-        if (opts.hasOwnProperty(prop)) {
-          defaults[prop] = opts[prop]
-        }
-      }
-    }
-    return JSON.parse(JSON.stringify(defaults))
+    return $.extend( true, defaults, opts )
   }
 
   /*
@@ -105,14 +101,29 @@
   */
 
   me.init = function () {
-    var css = defaults.css;
 
-    /* inject semantic-ui styles */
-    if(defaults.css.inject){
+    /* inject semantic-ui styles, if required */
+    var inject = defaults.css.inject;
+
+// semantic.min.css
+
+    /* if semantic-ui css file is not loaded, set inject to true if not already */
+    // for(var s in doc.styleSheets){
+    //   var sheet = doc.styleSheets[s];
+    //   inject = inject || (sheet && sheet.href > '') ? sheet.indexOf('')
+    // }
+    //
+
+
+    if(inject){
       for(var i in defaults.css.modules){
         $('head').append('<link rel="stylesheet" href="'
                 + compile(defaults.css.uri, {version: defaults.css.version,module: defaults.css.modules[i]})
                 + '" type="text/css" />');
+      }
+      /* inject custom css, if provided */
+      if(defaults.css.custom){
+        $('head').append('<link rel="stylesheet" href="' + defaults.css.custom + '" type="text/css" />');
       }
     }
 
@@ -127,13 +138,17 @@
       var cls = $(elem).attr('class') /* get element class */
 
       /* extract the github information from the element class string,  .. results in
-         an array ['username', 'repo name', 'function'], i.e. ["websemantics", "semanti", "stars"] */
-      var action = cls.slice(cls.indexOf(selector) + selector.length , cls.length)
+         an array ["github", "username", "repo name", "function"],
+            i.e. ["github", "websemantics", "semanti", "stars"] */
+      var action = cls.slice(cls.indexOf(selector) , cls.length)
         .split(' ')[0].split(defaults.delimiter)
 
-      /* if an action has at least two parts, username/repo add to list of uris */
-      if (action.length > 1) {
-        repos[action[0] + defaults.delimiter + action[1]] = {username: action[0], repo: action[1]}
+        /* hide the element first
+        $('.'+action.join(defaults.delimiter)).attr('style','visibility:hidden'); */
+
+      /* if an action has at least three parts, github, username/repo add to list of repos */
+      if (action.length > 2) {
+        repos[action[1] + defaults.delimiter + action[2]] = {username: action[1], repo: action[2]}
       }
     })
 
@@ -141,11 +156,13 @@
        and make necessary changes to html code (update stats, urls), .. fun fun! */
     for (var i in repos) {
       get(repos[i], function (repo, data) {
-        for (var action in defaults.actions) {
-          var cls = [defaults.cls, repo.username, repo.repo, action].join(defaults.delimiter)
-          $('.' + cls).attr('href', data['html_url'] + defaults.actions[action].uri)
-          if (defaults.actions[action].property) {
-            $('.' + cls + ' .label').text(data[defaults.actions[action].property] || 0)
+        if(data){
+          for (var action in defaults.actions) {
+            var cls = [defaults.cls, repo.username, repo.repo, action].join(defaults.delimiter)
+            $('.' + cls).attr('href', data['html_url'] + defaults.actions[action].uri)
+            if (defaults.actions[action].property) {
+              $('.' + cls + ' .label').text(data[defaults.actions[action].property] || 0)
+            }
           }
         }
       })
@@ -162,12 +179,19 @@
   */
 
   function get (repo, cb) {
-    $.ajax({
-      url: base_uri + repo.username + '/' + repo.repo,
-      complete: function (xhr) {
-        cb.call(null, repo, xhr.responseJSON)
-      }
-    })
+    try{
+      $.ajax({
+        url: base_uri + repo.username + '/' + repo.repo,
+        complete: function (xhr) {
+          cb.call(null, repo, xhr.responseJSON)
+        },
+        error: function(err){
+          log(( err && err.responseJSON ) ? err.responseJSON['message'] : 'There has been an error')
+        }
+      })
+    } catch(err){
+      log(err.message)
+    }
   }
 
   /*
@@ -186,10 +210,24 @@
     return template
   }
 
+  /*
+    Log a message to the console
+
+      Parameters:
+      - message (string): print out if in debug mode
+
+  */
+
+  function log (message) {
+    if(debug){
+      console.log(message);
+    }
+  }
+
   if (typeof $ === 'undefined') {
     console.error('Please install the latest jQuery!')
   } else {
-    me.init()
+    $(function(){me.init()});
   }
 
   return me
